b58c0536c3d634bb5d507b809735d31c72a2ed92
Ron Roff <rroff@roff.us>
2015-07-03 12:19:28 -0400

    Started Weather Content Provider: Updated Contract, Completed URIMatcher, Added Provider to Manifest.

--- app/src/main/java/us/roff/rroff/sunshine/data/WeatherProvider.java
+++ app/src/main/java/us/roff/rroff/sunshine/data/WeatherProvider.java
@@ -108,23 +108,21 @@ public class WeatherProvider extends ContentProvider {
         );
     }
 
-    /*
-        Students: Here is where you need to create the UriMatcher. This UriMatcher will
-        match each URI to the WEATHER, WEATHER_WITH_LOCATION, WEATHER_WITH_LOCATION_AND_DATE,
-        and LOCATION integer constants defined above.  You can test this by uncommenting the
-        testUriMatcher test within TestUriMatcher.
-     */
     static UriMatcher buildUriMatcher() {
         // 1) The code passed into the constructor represents the code to return for the root
         // URI.  It's common to use NO_MATCH as the code for this case. Add the constructor below.
-
+        final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = WeatherContract.CONTENT_AUTHORITY;
 
         // 2) Use the addURI function to match each of the types.  Use the constants from
         // WeatherContract to help define the types to the UriMatcher.
-
+        uriMatcher.addURI(authority, WeatherContract.PATH_WEATHER, WEATHER);
+        uriMatcher.addURI(authority, WeatherContract.PATH_WEATHER + "/*", WEATHER_WITH_LOCATION);
+        uriMatcher.addURI(authority, WeatherContract.PATH_WEATHER + "/*/#", WEATHER_WITH_LOCATION_AND_DATE);
+        uriMatcher.addURI(authority, WeatherContract.PATH_LOCATION, LOCATION);
 
         // 3) Return the new matcher!
-        return null;
+        return uriMatcher;
     }
 
     /*
