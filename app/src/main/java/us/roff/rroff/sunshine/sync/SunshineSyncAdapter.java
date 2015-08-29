0fd30a1034725068657d3470b6bb4132b1560ebc
Ron Roff <rroff@roff.us>
2015-08-29 16:54:04 -0400

    Stores server status as Shared Preference (using an @IntDef).

--- app/src/main/java/us/roff/rroff/sunshine/sync/SunshineSyncAdapter.java
+++ app/src/main/java/us/roff/rroff/sunshine/sync/SunshineSyncAdapter.java
@@ -20,6 +20,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.preference.PreferenceManager;
+import android.support.annotation.IntDef;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.text.format.Time;
@@ -33,6 +34,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Vector;
@@ -44,6 +47,15 @@ import us.roff.rroff.sunshine.data.WeatherContract;
 
 public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
     public final String LOG_TAG = SunshineSyncAdapter.class.getSimpleName();
+
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({LOCATION_STATUS_OK, LOCATION_STATUS_SERVER_DOWN, LOCATION_STATUS_SERVER_INVALID,
+            LOCATION_STATUS_UNKNOWN})
+    public @interface LocationStatus {}
+    public static final int LOCATION_STATUS_OK = 0;
+    public static final int LOCATION_STATUS_SERVER_DOWN = 1;
+    public static final int LOCATION_STATUS_SERVER_INVALID = 2;
+    public static final int LOCATION_STATUS_UNKNOWN = 3;
 
     // Interval at which to sync with the weather, in seconds.
     // 60 seconds (1 minute) * 180 = 3 hours
@@ -130,18 +142,20 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
 
             if (buffer.length() == 0) {
                 // Stream was empty.  No point in parsing.
-                return;
+                setLocationStatus(getContext(), LOCATION_STATUS_SERVER_DOWN);
+            } else {
+                forecastJsonStr = buffer.toString();
+                getWeatherDataFromJson(forecastJsonStr, locationQuery, provider);
             }
-            forecastJsonStr = buffer.toString();
-            getWeatherDataFromJson(forecastJsonStr, locationQuery, provider);
         } catch (IOException e) {
             Log.e(LOG_TAG, "Error ", e);
             // If the code didn't successfully get the weather data, there's no point in attempting
             // to parse it.
-            return;
+            setLocationStatus(getContext(), LOCATION_STATUS_SERVER_DOWN);
         } catch (JSONException e) {
             Log.e(LOG_TAG, e.getMessage(), e);
             e.printStackTrace();
+            setLocationStatus(getContext(), LOCATION_STATUS_SERVER_INVALID);
         } finally {
             if (urlConnection != null) {
                 urlConnection.disconnect();
@@ -461,15 +475,18 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
 
                     notifyWeather();
                 }
+                Log.d(LOG_TAG, "Sync Complete. " + inserted + " Inserted. "
+                        + deleted + " Deleted.");
+                setLocationStatus(getContext(), LOCATION_STATUS_OK);
             } catch (RemoteException e) {
                 Log.e(LOG_TAG, e.getMessage(), e);
                 e.printStackTrace();
+                setLocationStatus(getContext(), LOCATION_STATUS_UNKNOWN);
             }
-            Log.d(LOG_TAG, "Fetch Complete. " + inserted + " Inserted. " + deleted + " Deleted.");
-
         } catch (JSONException e) {
             Log.e(LOG_TAG, e.getMessage(), e);
             e.printStackTrace();
+            setLocationStatus(getContext(), LOCATION_STATUS_SERVER_INVALID);
         }
     }
 
@@ -520,5 +537,18 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
         }
 
         return id;
+    }
+
+    /**
+     * Sets the location status into shared preference.  This function should not be called from
+     * the UI thread because it uses commit to write to the shared preferences.
+     * @param context Context to get the PreferenceManager from.
+     * @param locationStatus The IntDef value to set
+     */
+    static private void setLocationStatus(Context context, @LocationStatus int locationStatus) {
+        SharedPreferences.Editor editor
+                = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putInt(context.getString(R.string.pref_location_status_key), locationStatus);
+        editor.commit();
     }
 }
